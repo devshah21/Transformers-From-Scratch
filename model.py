7f3faabc290e9f350141b2c0f20881e393896490
devshah21 <mail2devshah@gmail.com>
2023-06-07 19:06:05 -0400

    implemented initializer of transformer

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class SelfAttention(nn.module):
+    # refer to Notion for annotated code
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+        
+        assert (self.head_dim * heads == embed_size) # sanity check to ensure values allign
+        
+        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads*self.head_dim, embed_size)
+    
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+        
